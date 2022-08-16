2bba76eb38bf0957be0a27c52c36dd90540472c7
hyeonho <6190606@naver.com>
2022-08-16 21:30:52 +0900

    persist-message

--- src/main/java/com/hyunho9877/chatter/service/chat/implementation/ChatServiceImpl.java
+++ src/main/java/com/hyunho9877/chatter/service/chat/implementation/ChatServiceImpl.java
@@ -2,9 +2,11 @@ package com.hyunho9877.chatter.service.chat.implementation;
 
 import com.hyunho9877.chatter.domain.ChatMessage;
 import com.hyunho9877.chatter.domain.Exchange;
+import com.hyunho9877.chatter.repo.ChatMessageRepository;
 import com.hyunho9877.chatter.service.chat.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +17,18 @@ public class ChatServiceImpl implements ChatService {
 
     private static final String exchange = Exchange.EXCHANGE.getExchange();
     private final RabbitTemplate rabbitTemplate;
+    private final ChatMessageRepository messageRepository;
 
     @Override
     public void send(ChatMessage message) {
         log.info(exchange);
         log.info(exchange + "." + message.getReceiver());
         log.info(message.toString());
-        rabbitTemplate.convertAndSend(exchange, exchange + "." + message.getReceiver(), message);
+        try {
+            rabbitTemplate.convertAndSend(exchange, exchange + "." + message.getReceiver(), message);
+            messageRepository.save(message);
+        } catch (AmqpException e) {
+            log.error("message send failed : {}", message);
+        }
     }
 }
